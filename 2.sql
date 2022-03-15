7fc6894c655cd76807bcce627fcd18153ec44dd1
ahad111 <abdulahaduitian@gmail.com>
2022-03-15 11:59:15 +0500

    add sql file

---
+++ 2.sql
@@ -0,0 +1,26 @@
+
+
+
+SELECT * FROM sales_flat_order
+ORDER BY entity_id DESC
+
+SELECT * FROM powerapp_order
+ORDER BY entity_id DESC
+
+SELECT * FROM sales_flat_order
+ORDER BY entity_id DESC
+
+WHERE increment_id = 100833897
+
+SELECT * FROM sales_flat_order_item
+WHERE order_id = 832500
+
+
+
+SELECT * FROM powerapp_order
+
+ORDER BY entity_id DESC
+
+SELECT * FROM powerapp_order
+WHERE order_id = 100833897
+ORDER BY entity_id DESC
